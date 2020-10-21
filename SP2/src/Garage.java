2cc9e5e4cf7d55619688972316691b6a5c666ee3
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2020-10-21 11:23:09 +0200

    added garage class

--- SP2/src/Garage.java
+++ SP2/src/Garage.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Garage {
+    private ArrayList <Car> cars;
+
+    public Garage(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
+    private void calculateGreenTaxForParkedCars() {
+
+    }
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
 }
